b3e3de8d64d553776ba90da4b2ec81b7626eb026
adunham95 <adunham95@gmail.com>
2016-09-11 17:31:09 -0400

    added beer beer service and beer factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,50 +1,64 @@
 angular.module('app.controllers', [])
-  
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+app.constant('config', {
+  appNam: 'IonicBrew',
+  appVersion: "1.0",
+  apiUrl: "http://api.brewerydb.com/v2/?key=62335776d75e185980011577ab2440a4",
+  api_key: "62335776d75e185980011577ab2440a4"
+})
+
+  .controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('beerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('beerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams, BeerService) {
+
+      $scope.message = "Beer";
+      $scope.beerName = "";
+
+      $scope.fetchBeer = function () {
+        console.log("fetchBeer called");
+        BeerService.getBeer($scope.beerName)
+      }
 
 
-}])
-   
-.controller('breweryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('breweryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
-         
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    }])
+
+  .controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    function ($scope, $stateParams) {
 
 
-}])
- 
+    }])
--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,41 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+app.constant('config', {
+  appNam: 'IonicBrew',
+  appVersion: "1.0",
+  apiUrl: "http://api.brewerydb.com/v2/?key=62335776d75e185980011577ab2440a4",
+  api_key: "62335776d75e185980011577ab2440a4"
+})
 
-}])
+  .factory('BeerFactory', [function(){
 
-.service('BlankService', [function(){
+  }])
 
-}]);
+  .factory('BreweryFactory', [function(){
+
+  }])
+
+  .service('BeerService', [function($http){
+    console.log(constant.apiUrl);
+
+    var getBeer = function (beerName) {
+      console.log(beerName);
+      return $http.get("http://api.brewerydb.com/v2/beer/" + beerName + "/?key=" + constant.api_key)
+        .then(function sucessBeer(response) {
+          return response.data;
+        }, function errorCallBack(resposne) {
+
+        });
+    };
+
+    return {
+      getBeer: getBeer
+    }
+
+
+
+  }])
+
+  .service('BreweryService', [function(){
+
+  }]);
