855356e5442ede767048570db2085be0338f22cd
adunham95 <adunham95@gmail.com>
2016-09-21 16:37:05 -0400

    Added loading animatino

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,10 +10,10 @@ angular.module('app.controllers', [])
 
     }])
 
-  .controller('beerCtrl', ["$scope", "BeerFactory", "BeerService", "TestingFactory",// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('beerCtrl', ["$scope", "BeerFactory", "BeerService", "TestingFactory", "$ionicLoading",// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, BeerFactory, BeerService, TestingFactory) {
+    function ($scope, BeerFactory, BeerService, TestingFactory, $ionicLoading) {
       console.log("Inside Beer Controller");
 
       // function getBeers() {
@@ -40,14 +40,22 @@ angular.module('app.controllers', [])
         console.log("BeerNameSearch = "  + $scope.beerNameSearch);
         type = 'beer';
 
+        $ionicLoading.show({
+          template: '<ion-spinner></ion-spinner>',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 500,
+          showDelay: 100
+        });
+
         getBeers();
 
         function getBeers() {
           $scope.beer = {};
           TestingFactory.getBrews($scope.beerNameSearch, type)
-
             .then( function (response) {
               $scope.beer = response.data.data;
+              $ionicLoading.hide()
               var beerData= response.data.data;
               console.log($scope.beerNameSearch)
                 console.log($scope.beer);
